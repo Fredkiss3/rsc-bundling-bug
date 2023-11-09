22b07f28199f8eeac879cccc68841aebe55d39a8
Adrien KISSIE <fredkiss3@gmail.com>
2023-11-09 23:31:46 +0100

    don't include submit button in the bundle

--- app/submit-button.tsx
+++ app/submit-button.tsx
@@ -1,21 +1,22 @@
-"use client";
+// "use client";
 import * as React from "react";
-import { useFormStatus } from "react-dom";
-import { LoadingSpinner } from "./loading-spinner";
+// import { useFormStatus } from "react-dom";
+// import { LoadingSpinner } from "./loading-spinner";
 
 export type SubmitButtonProps = {
     children: React.ReactNode;
 };
 
 export function SubmitButton({ children }: SubmitButtonProps) {
-    const { pending } = useFormStatus();
+    // const { pending } = useFormStatus();
     return (
         <button
             type="submit"
             className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed"
-            disabled={pending}
+            // disabled={pending}
         >
-            {pending && <LoadingSpinner />} {children}
+            {children}
+            {/* {pending && <LoadingSpinner />} {children} */}
         </button>
     );
 }

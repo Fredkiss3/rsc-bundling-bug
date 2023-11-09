43b75a9dba256245ad11f3fb55e41e411b2c1635
Adrien KISSIE <fredkiss3@gmail.com>
2023-11-09 23:36:39 +0100

    include submit button

--- app/submit-button.tsx
+++ app/submit-button.tsx
@@ -1,22 +1,21 @@
-// "use client";
+"use client";
 import * as React from "react";
-// import { useFormStatus } from "react-dom";
-// import { LoadingSpinner } from "./loading-spinner";
+import { useFormStatus } from "react-dom";
+import { LoadingSpinner } from "./loading-spinner";
 
 export type SubmitButtonProps = {
     children: React.ReactNode;
 };
 
 export function SubmitButton({ children }: SubmitButtonProps) {
-    // const { pending } = useFormStatus();
+    const { pending } = useFormStatus();
     return (
         <button
             type="submit"
             className="inline-flex items-center px-4 py-2 font-semibold leading-6 text-sm shadow rounded-md text-white bg-indigo-500 hover:bg-indigo-400 transition ease-in-out duration-150 cursor-not-allowed"
-            // disabled={pending}
+            disabled={pending}
         >
-            {children}
-            {/* {pending && <LoadingSpinner />} {children} */}
+            {pending && <LoadingSpinner />} {children}
         </button>
     );
 }
